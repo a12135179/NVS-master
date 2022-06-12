43bff79ce3d2d0b5e4d3ac1a120a7425c94dde4a
HsiangKo <thumbg90214@gmail.com>
2022-06-13 07:22:19 +0800

    modify demo.py

--- demo.py
+++ demo.py
@@ -26,12 +26,17 @@ use_cuda = True
 
 cfgfile_RGB='./cfg/yolo-072812MotoCar.cfg'
 weightfile_RGB='./weight/12_MotoCar_0727.weights'
+
 cfgfile_TRM='./cfg/yolo-072812MotoCar.cfg'
 weightfile_TRM='./weight/0728_12Motocar_TRM.weights'
 
 imgfile_RGB='./data/RGB/RGB_yonghe6.avi'
 imgfile_TRM='./data/TRM/Thermal_yonghe6.avi'
 imgfile_fusion='./data/fusion/Crop_yonghe6.avi'
+
+output_RGB='./output/RGB/RGB_yonghe6.avi'
+output_TRM='./output/TRM/Thermal_yonghe6.avi'
+output_fusion='./output/fusion/Crop_yonghe6.avi'
 
 
 
@@ -48,8 +53,9 @@ if num_classes == 20:
 elif num_classes == 80:
     namesfile = 'data/coco.names'
 else:
-    namesfile = 'data/1104_obj.names'
+    namesfile = 'data/1104_obj.names'   #!!
 class_names = load_class_names(namesfile)
+
 
 
 TRM = Darknet(cfgfile_TRM)
@@ -67,11 +73,25 @@ else:
     namesfile = 'data/1104_obj.names'
 class_names = load_class_names(namesfile)
 
+writeVideo_flag = True
+if writeVideo_flag:
 
+    RGB_test = cv2.VideoCapture(imgfile_RGB)
+    T_test = cv2.VideoCapture(imgfile_TRM)
+    fusion_test = cv2.VideoCapture(imgfile_fusion)
 
-RGB_test = cv2.VideoCapture(imgfile_RGB)
-T_test = cv2.VideoCapture(imgfile_TRM)
-fusion_test = cv2.VideoCapture(imgfile_fusion)
+    if not RGB_test.isOpened() or not T_test.isOpened() or not fusion_test.isOpened():
+            raise IOError("Couldn't open webcam or video")
+    width           = int(RGB_test.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height          = int(RGB_test.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    video_FourCC    = cv2.VideoWriter_fourcc(*'XVID')
+    video_fps       = RGB_test.get(cv2.CAP_PROP_FPS)
+    video_size      = (width, height)
+    isOutput = True
+    if isOutput:
+        RGB_output      = cv2.VideoWriter(output_RGB, video_FourCC, video_fps, video_size)    
+        TRM_output      = cv2.VideoWriter(output_TRM, video_FourCC, video_fps, video_size)    
+        Fusion_output   = cv2.VideoWriter(output_fusion, video_FourCC, video_fps, video_size)
 
 while RGB_test.isOpened():
     t1 = time.time()
@@ -86,11 +106,17 @@ while RGB_test.isOpened():
     sized_TRM = cv2.resize(T_frame , (TRM.width, TRM.height))
     sized_TRM = cv2.cvtColor(sized_TRM, cv2.COLOR_BGR2RGB)
     boxes_fusion = do_detect_ye(TRM, RGB, sized_TRM, sized_RGB, 0.25, 0.4, use_cuda)
+    #if(int(boxes_fusion[0][6]) < 7 ):
+    #input(boxes_fusion[0][6][6])
     result_fusion = draw_bbox(f_frame, boxes_fusion[0], class_names=class_names, show_label=True)
+    #print(draw_bbox.class_names)
     t2 = time.time()
     print('-----------------------------------')
     print('       max and argmax : %f' % (t2 - t1))
     print('-----------------------------------')
-    cv2.imshow('Yolo demo', result_fusion)
+    #cv2.imshow('Yolo demo', result_fusion)
+        # RGB_output.write(result)
+    # TRM_output.write(result)
+    Fusion_output.write(result_fusion)
  #   cv2.imwrite("./result_fusion.jpg", img)
-    cv2.waitKey(1)
+    #cv2.waitKey(1)
